1aec86109f937e3b1dff7697cc577ca9eaecb3e8
returnik <roginogi271@gmail.com>
2025-12-05 14:05:52 +0100

    widok aplikacj2

--- app/src/main/java/pl/zs10zabrze/testoflagach/MainActivity.java
+++ app/src/main/java/pl/zs10zabrze/testoflagach/MainActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -15,5 +16,9 @@ public class MainActivity extends AppCompatActivity {
 
     public void ukryj(View view) {
         view.setVisibility(View.INVISIBLE);
+    }
+
+    public void ok(View view) {
+        Toast.makeText(this, "Ten kolor musi zostać w fladze POLSKI", Toast.LENGTH_SHORT).show();
     }
 }
